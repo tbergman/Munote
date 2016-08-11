af29fed0ff19da1c05472ee2d40ffe3605ac2725
mikeysax <michaelmerrittfarrell@gmail.com>
2016-08-11 19:59:52 -0400

    Added Note component. Measure component loops through notes in JSON, clef is also determined for 0 indexed measure.

--- src/js/components/Measure.js
+++ src/js/components/Measure.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Note from './Note';
 
 export default class Measure extends React.Component {
   render() {
+    const { attributes, note } = this.props.measure;
+
+    let notes = '';
+    if (note !== null && note !== undefined) {
+      notes = note.map((note, index) =>
+        <Note
+          key={index}
+          index={index}
+          type={note.type}
+          rest={note.rest}
+          voice={note.voice}
+          duration={note.duration}
+          length={note.length}
+        />
+      );
+    }
+
+    let cleff = '';
+    if (this.props.index === 0) {
+      cleff = attributes[0].clef[0].sign[0] === 'G' ?
+        'Treble Clef' : 'Bass Clef';
+    }
+
     return (
       <span className="measure col-xs-2">
-        M
+        {cleff}|{notes}|
       </span>
     );
   }
---
+++ tests/components/Note.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+// Component
+import Note from '../../src/js/components/Note';
+
+describe('Note Component', () => {
+
+  it('Waiting for Testing', () => {
+    return true;
+  });
+
+});
