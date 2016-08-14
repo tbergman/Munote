90275bc41b38cd3eab4a89f0d88ac18f10b60a5f
mikeysax <michaelmerrittfarrell@gmail.com>
2016-08-14 15:22:45 -0400

    Added instrument titles, organized part code, deleted measure and note components.

--- src/js/components/Part.js
+++ src/js/components/Part.js
@@ -14,34 +14,38 @@ export default class Part extends React.Component {
       this.refs.part,
       VF.Renderer.Backends.SVG
     );
-    // Configure the rendering context.
-    // renderer.resize(500, 200);
     const context = renderer.getContext();
     context.setFont(font.name, font.size, "").setBackgroundFillStyle("#eed");
 
-    // Notes
+    // Bar Params
     let barWidth = 200;
     let barX = 10;
     let barY = 0;
 
+    // Generate Staves with Notes according to JSON file.
     for(let i = 0; i < part.measures.length; i++) {
       // New Bar
       let stave;
       if (i === 0) {
+        // First Bar
         stave = new VF.Stave(barX, barY, barWidth);
         stave.addClef(part.clef).addTimeSignature(part.timeSignature);
       } else if (i >= 1) {
+        // Not First Bar
         stave = new VF.Stave(barX += 200, barY, barWidth);
+        // Add new time signature if time signature != piece signature or last bar signature
         if (part.timeSignature !== part.measures[i].timeSignature || part.measures[i - 1].timeSignature !== part.measures[i].timeSignature) {
           stave.addTimeSignature(part.measures[i].timeSignature);
         }
         renderer.resize(barWidth + barX, 200);
       }
-      console.log(stave.width);
-      console.log(stave.x);
-      console.log(stave.y);
 
-      stave.setEndBarType(VF.Barline.type.SINGLE);
+      if (part.measures.length - 1 === i) {
+        // If last put double barline
+        stave.setEndBarType(VF.Barline.type.END);
+      } else {
+        stave.setEndBarType(VF.Barline.type.SINGLE);
+      }
 
       // Connect it to the rendering context and draw!
       stave.setContext(context).draw();
@@ -53,8 +57,9 @@ export default class Part extends React.Component {
           duration: part.measures[i].notes[j].duration
         }))
       }
-
       console.log(notes);
+      VF.Formatter.FormatAndDraw(context, stave, notes);
+
       // Create a voice in Time signature and add above notes
       // let measureSig = part.measures[i].timeSignature;
       // let num_beats = parseInt(measureSig.substring(0, 1));
@@ -64,21 +69,20 @@ export default class Part extends React.Component {
       // voice.addTickables(notes);
 
       // Helper function to justify and draw a 4/4 voice
-      VF.Formatter.FormatAndDraw(context, stave, notes);
+
       // Format and justify the notes to 400 pixels.
       // let formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
 
       // Render voice
       // voice.draw(context, stave);
     }
-
-
   }
 
   render() {
     return (
-      <div ref="part" className="part">
-
+      <div>
+        <span className="instrument">Instrument: {this.props.part.instrument}</span>
+        <div ref="part" className="part"></div>
       </div>
     );
   }
